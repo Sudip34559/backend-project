25aac0205e1868c37c74ed4dfcd6dc8447ca6211
sudip <sudiptunga82@gmail.com>
2024-05-03 09:41:35 +0530

    utill code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(` listening on http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongodb connection failed: " + err);
+  });
 
 /*
 import express from 'express'
